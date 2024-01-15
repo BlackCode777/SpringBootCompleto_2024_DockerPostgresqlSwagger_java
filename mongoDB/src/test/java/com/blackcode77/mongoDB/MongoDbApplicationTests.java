1ee0b3277751c73fae6e2e50ab00814770779a7e
Anderson Martins <BlackCode77.1980@gmail.com>
2024-01-15 12:19:15 -0300

    Início do estudo do java com mongoDB

---
+++ mongoDB/src/test/java/com/blackcode77/mongoDB/MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackcode77.mongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
